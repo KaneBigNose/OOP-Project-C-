6abde8bed16ef5110fcbcf355d3205484248aed5
KaneBigNose <imddongs@naver.com>
2024-04-06 21:00:35 +0900

    2024-04-06-01
    문제 10-2

--- Practice/Project1/practice_main.cpp
+++ Practice/Project1/practice_main.cpp
@@ -12,60 +12,42 @@ public:
 	{
 		cout << '[' << xpos << ", " << ypos << ']' << endl;
 	}
-	friend Point operator+(const Point& pos1, const Point& pos2);
-	friend Point operator-(const Point& pos1, const Point& pos2);
-	Point& operator+=(const Point& copy)
+	Point& operator++()
 	{
-		xpos += copy.xpos;
-		ypos += copy.ypos;
+		xpos += 1;
+		ypos += 1;
 		return *this;
 	}
-	Point& operator-=(const Point& copy)
+	Point operator-()
 	{
-		xpos -= copy.xpos;
-		ypos -= copy.ypos;
+		xpos = -xpos;
+		ypos = -ypos;
 		return *this;
 	}
-	friend bool operator==(const Point& copy1, const Point& copy2);
-	friend bool operator!=(const Point& copy1, const Point& copy2);
+	friend Point& operator--(Point& ref);
+	friend Point operator~(Point& ref);
 };
 
-Point operator+(const Point& pos1, const Point& pos2)
+Point& operator--(Point& ref)
 {
-	Point pos(pos1.xpos + pos2.xpos, pos1.ypos + pos2.ypos);
-	return pos;
+	ref.xpos -= 1;
+	ref.ypos -= 1;
+	return ref;
 }
-Point operator-(const Point& pos1, const Point& pos2)
+Point operator~(Point& ref)
 {
-	Point pos(pos1.xpos - pos2.xpos, pos1.ypos - pos2.ypos);
-	return pos;
-}
-bool operator==(const Point& copy1, const Point& copy2)
-{
-	if (copy1.xpos == copy2.xpos && copy1.ypos == copy2.ypos) { return true; }
-	else { return false; }
-}
-bool operator!=(const Point& copy1, const Point& copy2)
-{
-	return !(copy1 == copy2);
+	int temp = ref.xpos;
+	ref.xpos = ref.ypos;
+	ref.ypos = temp;
+	return ref;
 }
 
 int main(void)
 {
-	Point pos1(20, 30);
-	Point pos2(5, 7);
-	Point pos3(5, 7);
-
-	(pos1 - pos2).ShowPosition();
-	(pos2 += pos3).ShowPosition();
-	pos1.ShowPosition();
+	Point pos1(1, 2);
+	Point pos2 = -pos1;
 	pos2.ShowPosition();
-
-	if (pos1 == pos2) { cout << "같음" << endl; }
-	else { cout << "다름" << endl; }
-	cout << "check" << endl;
-	if (pos2 != pos3) { cout << "다름" << endl; }
-	else { cout << "같음" << endl; }
-
+	Point pos3 = ~pos2;
+	pos3.ShowPosition();
 	return 0;
 }
